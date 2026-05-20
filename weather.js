a727aeed6a9f58fbffa45607e98cf03813e9755a
G584832025 <258483@st.takushoku-u.ac.jp>
2026-05-21 00:44:50 +0900

    宿題

--- weather.js
+++ weather.js
@@ -43,6 +43,22 @@ function finish() {
     console.log('Ajax 通信が終わりました');
 }
 
+
+
+let b = document.querySelector('button#btn');
+b.addEventListener('click', showSelectResult);
+
+function showSelectResult() {
+  let s = document.querySelector('select#Toshimei');
+  let idx = s.selectedIndex;  // idx 番目の option が選択された
+
+  let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
+  let o = os.item(idx);       // os の idx 番目の要素
+
+  console.log('ID ' + o.getAttribute('value'));  // id 属性を表示
+}
+
+
 ////////////////////////////////////////
 // 以下はグルメのデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
